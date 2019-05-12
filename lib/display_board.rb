95c70b53b42befba6a4c4e9ea2e93198c4add897
Min Kang <kangm914@gmail.com>
2019-05-12 14:38:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board  
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+names = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+def batch_badge_creator(names)
+    names.each do |name|
+        puts name 
 end
-# Write your code here.
-def badge_maker(name)
-    puts "Hello, my name is #{name}."
-end
-
-badge_maker(min)
